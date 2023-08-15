dd87a5d630ac8f8751de72a5af854b0e9313d0cf
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-08-15 23:13:20 +0900

    Dollarクラスの実装を追加

---
+++ app/Dollar.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+class Dollar {
+    public $amount;
+
+    public function __construct(int $amount) {
+        $this->amount = $amount;
+    }
+
+    public function times(int $multiplier) {
+        $this->amount *= $multiplier;
+    }
+}
--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use App\Dollar;
 
 final class MoneyTest extends TestCase
 {
