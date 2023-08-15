d6f164a02fe5acd2760e34660c2edddb0e198620
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-08-15 23:11:48 +0900

    最初のテストを作成

---
+++ tests/MoneyTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class MoneyTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testMultiplication(): void
+    {
+        $five = new Dollar(5);
+        $five->times(2);
+        $this->assertEquals(10, $five->amount);
+    }
+}
