4d06c5163efaa37f73d07f836755b0665a5e3c67
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-08-17 23:44:34 +0900

    timesメソッドを新しいDollarクラスを返すように変更
    副作用をなくすため。掛け算に使ったDollarインスタンスのamountを変化させずに済む

--- app/Dollar.php
+++ app/Dollar.php
@@ -2,14 +2,17 @@
 
 namespace App;
 
-class Dollar {
+class Dollar
+{
     public $amount;
 
-    public function __construct(int $amount) {
+    public function __construct(int $amount)
+    {
         $this->amount = $amount;
     }
 
-    public function times(int $multiplier) {
-        $this->amount *= $multiplier;
+    public function times(int $multiplier) : Dollar
+    {
+        return new Dollar($this->amount * $multiplier);
     }
 }
