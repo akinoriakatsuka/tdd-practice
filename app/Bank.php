ceb3f4661853fe3126a8e62b080d0f254adb8fc4
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-09-05 23:41:24 +0900

    Moneyにreduceを実装しExpressionに宣言を引き上げた

--- app/Bank.php
+++ app/Bank.php
@@ -9,7 +9,7 @@ class Bank
     public function reduce(Expression $source, string $to): Money
     {
         if ($source instanceof Money) {
-            return $source;
+            return $source->reduce($to);
         }
         $sum = $source;
         return $sum->reduce($to);
--- app/Expression.php
+++ app/Expression.php
@@ -5,4 +5,6 @@ declare(strict_types=1);
 namespace App;
 
 interface Expression
-{}
+{
+    public function reduce(string $to): Money;
+}
