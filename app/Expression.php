608aecdf16badb6c0bfa77810751700bb87fa552
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-09-09 00:13:08 +0900

    異なる通貨同士の足し算のテストと実装を追加

--- app/Expression.php
+++ app/Expression.php
@@ -6,5 +6,6 @@ namespace App;
 
 interface Expression
 {
+    public function plus(self $addend): self;
     public function reduce(Bank $bank, string $to): Money;
 }
--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -84,4 +84,13 @@ final class MoneyTest extends TestCase
         $this->assertSame(2, $bank->rate('CHF','USD'));
     }
 
+    public function testMixedAddtion(): void
+    {
+        $fiveBucks = Money::dollar(5);
+        $tenFrancs = Money::franc(10);
+        $bank = new Bank();
+        $bank->addRate('CHF', 'USD', 2);
+        $result = $bank->reduce($fiveBucks->plus($tenFrancs), 'USD');
+        $this->assertTrue(Money::dollar(10)->equals($result));
+    }
 }
