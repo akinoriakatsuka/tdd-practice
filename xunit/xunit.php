d432c2cb259eec37d17595d324e1862613c23178
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-09-10 18:55:45 +0900

    テストメソッドを起動する仕事とテストメソッドが実行されたか記録する仕事を担うクラスを分離

--- xunit/xunit.php
+++ xunit/xunit.php
@@ -7,22 +7,30 @@ assert(!$test->wasRun);
 $test->run();
 assert($test->wasRun);
 
-class WasRun
+class TestCase
 {
-    public $wasRun;
     public $name;
     public function __construct($name)
     {
-        $this->wasRun = null;
         $this->name = $name;
-    }
-    public function testMethod()
-    {
-        $this->wasRun = 1;
     }
     public function run()
     {
         $func = $this->name;
         $this->$func();
+    }
+}
+
+class WasRun extends TestCase
+{
+    public $wasRun;
+    public function __construct($name)
+    {
+        $this->wasRun = null;
+        parent::__construct($name);
+    }
+    public function testMethod()
+    {
+        $this->wasRun = 1;
     }
 }
